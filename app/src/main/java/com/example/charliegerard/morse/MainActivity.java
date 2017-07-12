7a6da1e8c3f668c5cde7854115a4f936dbaec9a6
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-07-12 15:22:38 +0100

    started mapping letters to morse code

--- app/src/main/java/com/example/charliegerard/morse/MainActivity.java
+++ app/src/main/java/com/example/charliegerard/morse/MainActivity.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +20,10 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.switchBtn) Button toggleBtnOff;
+    @BindView(R.id.translateBtn) Button translateButton;
+    @BindView(R.id.inputText) EditText inputField;
+
+    HashMap<String, String> morseMap = new HashMap<String, String>();
 
     private MyCameraImpl cameraImpl;
     public static boolean isFlashlightOn = false;
@@ -24,6 +33,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setupMorseMap();
+    }
+
+    private void setupMorseMap(){
+        morseMap.put("a", ". -");
+        morseMap.put("b", "- . . .");
+        morseMap.put("c", "- . - .");
+        morseMap.put("d", "- . .");
+        morseMap.put("e", ".");
+        morseMap.put("f", ". . - .");
+        morseMap.put("g", "- - .");
+        morseMap.put("h", ". . . .");
+        morseMap.put("i", ". .");
+        morseMap.put("j", ". - - -");
+        morseMap.put("k", "- . -");
+        morseMap.put("l", ". - . .");
+        morseMap.put("m", "- -");
+        morseMap.put("n", "- .");
+        morseMap.put("o", "- - -");
+        morseMap.put("p", ". - - .");
+        morseMap.put("q", "- - . -");
+        morseMap.put("r", ". - .");
+        morseMap.put("s", ". . .");
+        morseMap.put("t", "-");
+        morseMap.put("u", ". . -");
+        morseMap.put("v", ". . . -");
+        morseMap.put("w", ". - -");
+        morseMap.put("x", "- . . -");
+        morseMap.put("y", "- . - -");
+        morseMap.put("z", "- - . .");
     }
 
     private void setupCameraImpl(){
@@ -42,6 +81,20 @@ public class MainActivity extends AppCompatActivity {
         } else {
             toggleBtnOff.setBackgroundResource(R.drawable.off);
         }
+    }
+
+    @OnClick(R.id.translateBtn)
+    public void translate(){
+        String message = inputField.getText().toString();
+
+        for(int item = 0; item < message.length(); item++){
+            String character = String.valueOf(message.charAt(item));
+
+            if(morseMap.containsKey(character)){
+                Log.d("text", morseMap.get(character));
+            }
+        }
+
     }
 
     @Override
