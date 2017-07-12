b3aac3de85ff7544e67eb2d9254dd1fdae041d8c
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-07-12 21:34:22 +0100

    morse to flashlights working

--- app/src/main/java/com/example/charliegerard/morse/MainActivity.java
+++ app/src/main/java/com/example/charliegerard/morse/MainActivity.java
@@ -2,19 +2,13 @@ package com.example.charliegerard.morse;
 
 import android.hardware.Camera;
 import android.media.Image;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,18 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     HashMap<String, String> morseMap = new HashMap<String, String>();
 
-    private Handler handler;
-    private int interval = 1000;
-
     private int oneTimeUnit = 500;
     private int dotUnitDuration = oneTimeUnit;
     private int dashUnitDuration = oneTimeUnit * 3;
     private int gapInCharacter = oneTimeUnit;
     private int gapBetweenLetters = oneTimeUnit * 3;
     private int getGapBetweenWords = oneTimeUnit * 7;
-
-    private int currentDuration;
-    List<Integer> durations = new ArrayList<Integer>();
 
     private MyCameraImpl cameraImpl;
     public static boolean isFlashlightOn = false;
@@ -50,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        handler = new Handler();
 
         setupMorseMap();
     }
@@ -87,8 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupCameraImpl(){
         cameraImpl = new MyCameraImpl(this);
-        // The following line was triggering the flashlight by default when the app opens.
-        // cameraImpl.enableFlashlight();
     }
 
     @OnClick(R.id.switchBtn)
@@ -105,82 +89,66 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.translateBtn)
     public void translate(){
-        String message = inputField.getText().toString();
-
         // At the moment, only working with messages made of 1 word.
-
-        for(int item = 0; item < message.length(); item++){
-            // Each character in message.
-            String character = String.valueOf(message.charAt(item));
-
-            if(morseMap.containsKey(character)){
-                // Morse value for each character in message.
-                String morseValue = morseMap.get(character);
-
-                for(int index = 0; index < morseValue.length(); index++){
-                    // Each character in morse value;
-                    String singleCharacterInMorseValue = String.valueOf(morseValue.charAt(index));
-
-                    if(singleCharacterInMorseValue.equals("-")){
-                        // dashUnitDuration
-//                        currentDuration = dashUnitDuration;
-
-                        durations.add(dashUnitDuration);
-
-                    } else if(singleCharacterInMorseValue.equals(".")){
-                        // dotUnitDuration
-//                        currentDuration = dotUnitDuration;
-                        Log.d("there", "should be .");
-                        durations.add(dotUnitDuration);
-
-                    } else if(singleCharacterInMorseValue.equals(" ")){
-                        // gapInCharacter
-//                        currentDuration = gapInCharacter;
-
-                        durations.add(gapInCharacter);
-                    }
-                }
-            }
-        }
-        startTranslation(durations);
-    }
-
-    Runnable mStatusChecker = new Runnable() {
-        @Override
-        public void run() {
-
-            handler.postDelayed(mStatusChecker, currentDuration);
-        }
-    };
-
-    public void startTranslating(){
-        mStatusChecker.run();
-    }
-
-    public void stopRepeatingTask(){
-        handler.removeCallbacks(mStatusChecker);
-    }
-
-    private void startTranslation(List durations){
-//        for(int index = 0; index < durations.size(); index++ ){
-//            Log.d("durations", String.valueOf(durations.get(index)));
-//        }
 
         new Thread(morse).start();
     }
 
     private Runnable morse = new Runnable(){
 
+        public void mapMessageToMorse(){
+            String message = inputField.getText().toString();
+
+            //Probably could be refactored;
+
+            for(int item = 0; item < message.length(); item++){
+                // Each character in message.
+                String character = String.valueOf(message.charAt(item));
+
+                if(morseMap.containsKey(character)){
+                    // Morse value for each character in message.
+                    String morseValue = morseMap.get(character);
+
+                    for(int index = 0; index < morseValue.length(); index++){
+                        // Each character in morse value;
+                        String singleCharacterInMorseValue = String.valueOf(morseValue.charAt(index));
+
+                        if(singleCharacterInMorseValue.equals("-")){
+
+                            try{
+                                cameraImpl.toggleFlashlight(true);
+                                Thread.sleep(dashUnitDuration);
+                            } catch(InterruptedException e){
+                                Log.e("Err", "Interruption exception " + e.getMessage());
+                            }
+
+                        } else if(singleCharacterInMorseValue.equals(".")){
+
+                            try{
+                                cameraImpl.toggleFlashlight(true);
+                                Thread.sleep(dotUnitDuration);
+                            } catch(InterruptedException e){
+                                Log.e("Err", "Interruption exception " + e.getMessage());
+                            }
+
+                        } else if(singleCharacterInMorseValue.equals(" ")){
+
+                            try{
+                                cameraImpl.toggleFlashlight(false);
+                                Thread.sleep(gapInCharacter);
+                            } catch(InterruptedException e){
+                                Log.e("Err", "Interruption exception " + e.getMessage());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         @Override
         public void run() {
-            try{
-                cameraImpl.toggleFlashlight(true);
-                Thread.sleep(5000);
-                cameraImpl.toggleFlashlight(false);
-                Thread.sleep(1000);
-            } catch(InterruptedException e){
-                Log.e("Err", "Interruption exception " + e.getMessage());
-            }
+            mapMessageToMorse();
+            cameraImpl.toggleFlashlight(false);
         }
     };
 
@@ -190,11 +158,5 @@ public class MainActivity extends AppCompatActivity {
         if(cameraImpl == null){
             setupCameraImpl();
         }
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        stopRepeatingTask();
     }
 }
