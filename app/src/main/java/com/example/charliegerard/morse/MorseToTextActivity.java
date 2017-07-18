59f1ec08cce088212a3f557f5e62b7a9a03608b1
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-07-18 12:19:46 +0100

    get camera preview

--- app/src/main/java/com/example/charliegerard/morse/MorseToTextActivity.java
+++ app/src/main/java/com/example/charliegerard/morse/MorseToTextActivity.java
@@ -1,10 +1,15 @@
 package com.example.charliegerard.morse;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
 public class MorseToTextActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,23 @@ public class MorseToTextActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
