f7b056a1c4bfb9d7a92b10b29cf0a775558da4e1
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-07-18 14:33:06 +0100

    make text block smaller than video feed

--- app/src/main/java/com/example/charliegerard/morse/MorseToTextActivity.java
+++ app/src/main/java/com/example/charliegerard/morse/MorseToTextActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public class MorseToTextActivity extends AppCompatActivity {
 
