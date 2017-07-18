b28ed008fd741ef705a6731a2d019fc718be18de
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-07-18 11:22:51 +0100

    add toolbar when selecting 'text to morse'

--- app/src/main/java/com/example/charliegerard/morse/TextToMorseActivity.java
+++ app/src/main/java/com/example/charliegerard/morse/TextToMorseActivity.java
@@ -2,6 +2,7 @@ package com.example.charliegerard.morse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TextToMorseActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class TextToMorseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_to_morse);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
